8c5f5e7ffb3d555548c8490be6e754aca94b076d
Fighting <xing.suifeng@gmail.com>
2018-03-27 00:45:08 +0800

    对接服务端api，实现两个json窗口数据同步

--- mock/app1.js
+++ mock/app1.js
@@ -1,1 +1,4 @@
-module.exports={"/api/test2":{"a":1,"b":222},"/api/lin/test":{"a":1,"b":2,"c":[1,2,3]}}
+module.exports = {
+  "/api/test2": { a: 1, b: 222 },
+  "/api/lin/test": { a: 1, b: 2, c: [1, 2, 3] }
+};
