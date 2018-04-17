393d9e57a53acd39f10c2c4fe58e2c86c6176a9c
Elity <xing.suifeng@gmail.com>
2018-04-17 19:50:01 +0800

    测试watch不再需要定时器，但是测试进程无法自动终止，暂不清楚原因

--- test/watch.test.js
+++ test/watch.test.js
@@ -2,52 +2,33 @@ const expect = require("chai").expect;
 const watch = require("../src/watch");
 const path = require("path");
 const fs = require("fs");
-const sinon = require("sinon");
-
-function waitFor(spies, fn) {
-  function isSpyReady(spy) {
-    return Array.isArray(spy) ? spy[0].callCount >= spy[1] : spy.callCount;
-  }
-  function finish() {
-    clearInterval(intrvl);
-    clearTimeout(to);
-    fn();
-    fn = Function.prototype;
-  }
-  let intrvl = setInterval(function() {
-    if (spies.every(isSpyReady)) finish();
-  }, 5);
-  let to = setTimeout(finish, 3500);
-}
+//const sinon = require("sinon");
 
 describe("watch test", function() {
-  it("should emit `create` event when file was changed", function(done) {
+  it("should emit `create`,`delete` event when file was create and delete", function(done) {
     let file = path.resolve("./test/api/create.js");
-    let spy = sinon.spy();
-    watch(file)
-      .on("create", spy)
-      .on("ready", function() {
-        expect(spy.notCalled).to.be.ok;
-        fs.writeFileSync(file, Date.now());
-        setTimeout(() => {
-          expect(spy.calledOnce).to.be.ok;
-          done();
-        }, 100);
+    let wc = watch(file);
+    wc.on("ready", function() {
+      wc.on("create", function() {
+        fs.unlinkSync(file);
       });
+      wc.on("delete", function() {
+        wc.close();
+        done();
+      });
+      fs.writeFileSync(file, Date.now());
+    });
   });
 
   it("should emit `change` event when file was changed", function(done) {
     let file = path.resolve("./test/api/apitest.js");
-    let spy = sinon.spy();
-    watch(file)
-      .on("change", spy)
-      .on("ready", function() {
-        expect(spy.notCalled).to.be.ok;
-        fs.writeFileSync(file, Date.now());
-        setTimeout(() => {
-          expect(spy.calledOnce).to.be.ok;
-          done();
-        }, 100);
+    let wc = watch(file);
+    wc.on("ready", function() {
+      wc.on("change", function() {
+        wc.close();
+        done();
       });
+      fs.writeFileSync(file, Date.now());
+    });
   });
 });
