d220e69e8ad4b89488c55696b606e9e4ae827a5c
Elity <xing.suifeng@gmail.com>
2018-04-18 22:01:36 +0800

    chokidar一个关于glob的bug

--- src/watch.js
+++ src/watch.js
@@ -1,23 +1,17 @@
 const chokidar = require("chokidar");
 
 function watch(globPath) {
-  const watcher = chokidar.watch(globPath, { ignored: /node_modules/ });
-  const eventMap = {
-    create: "add",
-    change: "change",
-    delete: "unlink",
-    ready: "ready"
-  };
+  // chokidar.watch 传入glob类型地址的时候 不能是 windows风格的斜杠
+  const watcher = chokidar.watch(globPath.replace(/\\/g, "/"), {
+    ignored: /node_modules/
+  });
   return {
     on(eventNames, fn) {
       eventNames
         .split(/\s+/)
         .filter(name => name)
         .map(name => name.toLowerCase())
-        .forEach(
-          name =>
-            eventMap[name] && watcher.on(eventMap[name], fn.bind(null, name))
-        );
+        .forEach(name => name && watcher.on(name, fn.bind(null, name)));
       return this;
     },
     close() {
